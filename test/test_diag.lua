d1f0166364ad822d09e18b65bae2aef26cd871e1
Rouboufy <213021409+Rouboufy@users.noreply.github.com>
2026-05-27 14:27:22 +0900

    chore: remove tracked test files and add test/ to gitignore

--- test/test_diag.lua
+++
@@ -1,9 +0,0 @@
-local n = require("diagnostics.norminette")
-print("Manually running norminette diagnostics...")
-n.run()
-vim.wait(2000, function() return false end) -- Wait for job to finish
-local diagnostics = vim.diagnostic.get(0)
-print("Found " .. #diagnostics .. " diagnostics.")
-for _, d in ipairs(diagnostics) do
-    print("Diag: [" .. d.lnum .. ":" .. d.col .. "] " .. d.message)
-end
